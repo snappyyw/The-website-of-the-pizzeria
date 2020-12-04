d2f8cae369022b897f8e56de0ffcebddb3fc9631
snappyyw <snippymr@yandex.ru>
2020-12-04 17:43:32 +0300

    Pizza component, json requests

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,13 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setItems(data.pizzas);
+    });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={items} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
